55a4c2172df509c7345f40ebf7dfdd9a96de7661
Valerie Joplin <89474266+valeriejoplin@users.noreply.github.com>
2022-10-05 13:50:41 -0500

    Update header.php

--- header.php
+++ header.php
@@ -10,7 +10,7 @@
   <div class="container-fluid">
     <a class="navbar-brand" href="/">
           <div class="logo-image">
-            <img src="https://i.pinimg.com/originals/44/ec/f4/44ecf4af02e3b89998b3b2e95da424e6.jpg" class="img-fluid">
+            <img src="https://i.pinimg.com/originals/44/ec/f4/44ecf4af02e3b89998b3b2e95da424e6.jpg"  width="20" height = "20" class="img-fluid">
       </div>
       </a>
 <nav class="navbar navbar-expand-lg bg-light">
