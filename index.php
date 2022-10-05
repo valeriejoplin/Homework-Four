eeb88ddc3270ea4af7f9014592acbbade93aed68
Valerie Joplin <89474266+valeriejoplin@users.noreply.github.com>
2022-10-05 14:07:51 -0500

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ require_once ("header.php");
 <h6> Use the heading bar to navigate our website!</h6>
 <img src="https://theceliacmd.com/wp-content/uploads/2019/10/people-laughing-in-restaurant-2508x1254.jpg" class="img-fluid" alt="...">
 </body>
-<h1> Join us for our "World-Famous Burgers and Fries" </h1>
+<h2> Join us for our "World-Famous Burgers and Fries" ! </h2>
 <img src="https://www.montfordinn.com/wp-content/uploads/2016/09/food_norman.png" class="img-fluid" alt="...">
 
 <?php
