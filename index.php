516b7e4a01699f7e2475b7559b1d04c90b105b4c
Valerie Joplin <89474266+valeriejoplin@users.noreply.github.com>
2022-10-05 14:02:07 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,10 @@
 <?php
 require_once ("header.php");
 ?>
-
+<body>
+<h1> Welcome to The Eatery's website </h1>
+<img src="https://theceliacmd.com/wp-content/uploads/2019/10/people-laughing-in-restaurant-2508x1254.jpg" class="img-fluid" alt="...">
+</body>
 
 
 <?php
