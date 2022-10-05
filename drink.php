859a85d993c3d7af4628c00c46d65a0621dcc7f7
Valerie Joplin <89474266+valeriejoplin@users.noreply.github.com>
2022-10-05 14:36:47 -0500

    Update drink.php

--- drink.php
+++ drink.php
@@ -22,7 +22,7 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "SELECT * from Drinks";
+$sql = "SELECT * from DrinkMenu";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
