70dca7a5fea16a2b952e36a96a737cf473263644
Valerie Joplin <89474266+valeriejoplin@users.noreply.github.com>
2022-10-05 14:07:13 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,13 @@
 <?php
 require_once ("header.php");
 ?>
-<body>
-<h1 style="text-align:center";> Welcome to The Eatery </h1>
+<body style ="text-align:center";>
+<h1> Welcome to The Eatery </h1>
+<h6> Use the heading bar to navigate our website!</h6>
 <img src="https://theceliacmd.com/wp-content/uploads/2019/10/people-laughing-in-restaurant-2508x1254.jpg" class="img-fluid" alt="...">
 </body>
-
+<h1> Join us for our "World-Famous Burgers and Fries" </h1>
+<img src="https://www.montfordinn.com/wp-content/uploads/2016/09/food_norman.png" class="img-fluid" alt="...">
 
 <?php
 require_once ("footer.php");
