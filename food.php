b149cbeb4f8990cd9319fad9c24c6dbaaedb164f
Valerie Joplin <89474266+valeriejoplin@users.noreply.github.com>
2022-10-06 23:33:32 -0500

    Update food.php

--- food.php
+++ food.php
@@ -1,6 +1,53 @@
 <?php
 require_once("header.php")
 ?>
+ <body>
+<table class="table table-striped">
+  <thead>
+    <tr>
+      <th>ID</th>
+      <th>Name</th>
+      <th>Price</th>
+      <th>Description</th>
+      <th>Default Side</th>
+    </tr>
+  </thead>
+  <tbody>
+    <?php
+$servername = "localhost";
+$username = "valeriej_databaseuser";
+$password = "tI_*dXAL^r[(";
+$dbname = "valeriej_homework4";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT * FoodMenu";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+
+  while($row = $result->fetch_assoc()) {
+?>
+  <tr>
+    <td><?=$row["FoodID"]?></td>
+    <td><?=$row["ItemName"]?></td>
+    <td><?=$row["Price"]?></td>
+    <td><?=$row["Description"]?></td>
+    <td><?=$row["DefaultSide"]?></td>
+   
+  </tr>
+<?php
+  }
+} else {
+  echo "0 results";
+}
+$conn->close();
+?>
+  </tbody>
+    </table>
 
 <?php
 require_once("footer.php")
