9e75388f37fc3ef8456880b4c3cd860fb53993a6
Valerie Joplin <89474266+valeriejoplin@users.noreply.github.com>
2022-10-10 14:18:27 -0500

    Update locations.php

--- locations.php
+++ locations.php
@@ -21,7 +21,7 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "SELECT LocationID, City from Locations";
+$sql = "SELECT LocationID, City from Location";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
