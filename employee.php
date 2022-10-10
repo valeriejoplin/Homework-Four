77ce07b2cbe4b8c07eea628dc34bccb22aea6eb6
Valerie Joplin <89474266+valeriejoplin@users.noreply.github.com>
2022-10-10 13:50:11 -0500

    Update employee.php

--- employee.php
+++ employee.php
@@ -1,9 +1,52 @@
 <?php
 require_once("header.php");
 ?>
+<body>
+<table class="table table-striped">
+  <thead>
+    <tr>
+      <th>ID</th>
+      <th>First Name</th>
+      <th>Last Name</th>
+      <th>Date of Hire</th>
+      <th>Position</th>
+    </tr>
+  </thead>
+  <tbody>
+    <?php
+$servername = "localhost";
+$username = "valeriej_databaseuser";
+$password = "tI_*dXAL^r[(";
+$dbname = "valeriej_homework4";
 
+$conn = new mysqli($servername, $username, $password, $dbname);
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
 
+$sql = "SELECT * from Employees";
+$result = $conn->query($sql);
 
+if ($result->num_rows > 0) {
+
+  while($row = $result->fetch_assoc()) {
+?>
+  <tr>
+    <td><?=$row["EmployeeID"]?></td>
+    <td><?=$row["FirstName"]?></td>
+    <td><?=$row["LastName"]?></td>
+    <td><?=$row["HireDate"]?></td>
+    <td><?=$row["Position"]?></td>
+  </tr>
+<?php
+  }
+} else {
+  echo "0 results";
+}
+$conn->close();
+?>
+  </tbody>
+    </table>
 <?php
 require_once("footer.php");
 ?>
