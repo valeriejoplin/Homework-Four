129877f55af6a1f27dac6344a91bea2b85662007
Valerie Joplin <89474266+valeriejoplin@users.noreply.github.com>
2022-10-10 13:31:30 -0500

    Update customers.php

--- customers.php
+++ customers.php
@@ -1,6 +1,51 @@
 <?php
 require_once("header.php")
 ?>
+ <body>
+<table class="table table-striped">
+  <thead>
+    <tr>
+      <th>ID</th>
+      <th>Name</th>
+      <th>Favorite Menu Item</th>
+      <th>Optional Notes</th>
+    </tr>
+  </thead>
+  <tbody>
+    <?php
+$servername = "localhost";
+$username = "valeriej_databaseuser";
+$password = "tI_*dXAL^r[(";
+$dbname = "valeriej_homework4";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT DrinkID, ItemName, Price, Description from DrinkMenu";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+
+  while($row = $result->fetch_assoc()) {
+?>
+  <tr>
+    <td><?=$row["CustomerID"]?></td>
+    <td><?=$row["FirstName"]?></td>
+    <td><?=$row["LastName"]?></td>
+    <td><?=$row["FavoriteItem"]?></td>
+    <td><?=$row["Notes"]?></td>
+  </tr>
+<?php
+  }
+} else {
+  echo "0 results";
+}
+$conn->close();
+?>
+  </tbody>
+    </table>
 <?php
 require_once("footer.php")
 ?>
