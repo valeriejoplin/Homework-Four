575a4ce749be653d09c414fd82c66e4d55630c6d
Valerie Joplin <89474266+valeriejoplin@users.noreply.github.com>
2022-10-10 14:09:27 -0500

    Update location-details.php

--- location-details.php
+++ location-details.php
@@ -1,10 +1,51 @@
 <?php
 require_once("header.php");
 ?>
+<body>
+    <h1>Location Details</h1>
+<table class="table table-striped">
+  <thead>
+    <tr>
+      <th>ID</th>
+      <th>City</th>
+      <th>State</th>
+      <th>Address</th>
+    </tr>
+  </thead>
+  <tbody>
+    <?php
+$servername = "localhost";
+$username = "valeriej_databaseuser";
+$password = "tI_*dXAL^r[(";
+$dbname = "valeriej_homework4";
 
+$conn = new mysqli($servername, $username, $password, $dbname);
 
+   if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+$iid = $_GET['LocationID'];
+$sql = "SELECT * FROM Locations WHERE LocationID=" . $iid;
+    $result = $conn->query($sql);
 
-
+if ($result->num_rows > 0) {
+  while($row = $result->fetch_assoc()) {
+?>
+  <tr>
+    <td><?=$row["LocationId"]?></td>
+    <td><?=$row["City"]?></td>
+    <td><?=$row["State"]?></td>
+    <td><?=$row["Address"]?></td>
+  </tr>
+<?php
+  }
+} else {
+  echo "0 results";
+}
+$conn->close();
+?>
+  </tbody>
+    </table>
 <?php
 require_once("footer.php");
 ?>
