acda669abb4d7039e0c1b80ca2f2d00181a092ea
Valerie Joplin <89474266+valeriejoplin@users.noreply.github.com>
2022-10-10 13:38:00 -0500

    Update index.php

--- index.php
+++ index.php
@@ -12,7 +12,36 @@ require_once ("header.php");
 <img src="https://www.montfordinn.com/wp-content/uploads/2016/09/food_norman.png" class="img-fluid" alt="...">
 <h2> Happy Hour is everyday from 3-5 </h2>
 <h6> Enjoy half priced appetizers and specialty drinks! </h6>
-
+<table class="table">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">Drink</th>
+      <th scope="col">Regular Price</th>
+      <th scope="col">Happy Hour Price</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Millie's Margarita</td>
+      <td>$11</td>
+      <td>$5.50</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Avi's Aperol Spritz</td>
+      <td>$12</td>
+      <td>$6</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td>Jacque's Julep</td>
+      <td>$13</td>
+      <td>$6.50</td>
+    </tr>
+  </tbody>
+</table>
 
 <?php
 require_once ("footer.php");
