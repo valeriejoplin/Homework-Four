154a674b5f3a3e0437b5c570d95524d6426e5c99
Valerie Joplin <89474266+valeriejoplin@users.noreply.github.com>
2022-10-12 13:56:57 -0500

    Update food.php

--- food.php
+++ food.php
@@ -14,7 +14,7 @@ if ($conn->connect_error) {
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
   switch ($_POST['saveType']) {
     case 'Add':
-      $sqlAdd = "insert into FoodMenu (ItemName), (Price) values (?)";
+      $sqlAdd = "insert into FoodMenu (ItemName) value (?)";
       $stmtAdd = $conn->prepare($sqlAdd);
       $stmtAdd->bind_param("s", $_POST['iItem']);
       $stmtAdd->execute();
@@ -61,7 +61,41 @@ if ($result->num_rows > 0) {
     <td><?=$row["Price"]?></td>
     <td><?=$row["Description"]?></td>
     <td><?=$row["DefaultSide"]?></td>
-   
+      <td> 
+          <button type ="button" class="btn" data-bs-toggle="modal" data-bs-target="#editFood<?-$row["FoodID"]?>">
+              Edit
+          </button>
+          <div class ="modal fade" id="editFood<?=$row["FoodID"]?>" data-bs-background="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="editItem<?=$row["FoodID"]?>Label" aria-hidden="true">
+            <div class="modal-dialog">
+               <div class="modal-content">
+                 <div class="modal-header">
+                     <h1 class="modal-title fs-5" id="editItem<?=$row["FoodID"]?>Label">Edit Menu Items</h1>
+                      <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                      <form method="post" action="">
+                        <div class="mb-3">      
+                       <label for="editItem<?=$row["FoodItem"]?>Name" class="form-label">Name</label> 
+<input type="text" class="form-control" id="editItem<?=$row["FoodID"]?>Name" aria-describedby="editItem<?=$row["FoodID"]?>Help" name="iItem" value="<?=$row['ItemName']?>">
+                          <div id="editItem<?=$row["FoodID"]?>Help" class="form-text">Enter the item's name.</div>
+                        </div>
+                        <input type="hidden" name="iid" value="<?=$row['FoodID']?>">
+                        <input type="hidden" name="saveType" value="Edit">
+                        <button type="submit" class="btn btn-primary">Submit</button>
+                      </form>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </td>
+            <td>
+              <form method="post" action="">
+                <input type="hidden" name="iid" value="<?=$row["FoodID"]?>" />
+                <input type="hidden" name="saveType" value="Delete">
+                <button type="submit" class="btn" onclick="return confirm('Are you sure?')">Delete</button>
+              </form>
+            </td>                        
+
   </tr>
 <?php
   }
