64a3339bdee1694688f447b7184726716e4c3662
Valerie Joplin <89474266+valeriejoplin@users.noreply.github.com>
2022-10-17 13:47:41 -0500

    Update food-add.php

--- food-add.php
+++ food-add.php
@@ -6,7 +6,16 @@ require_once("header.php");
   <div class="mb-3">
     <label for="ItemName" class="form-label">Item</label>
     <input type="text" class="form-control" id="ItemName" aria-describedby="nameHelp" name="iItem">
+    <input type="text" class="form-control" id="Price" aria-describedby="priceHelp" name="iItem">
+    <input type="text" class="form-control" id="Description" aria-describedby="descHelp" name="iItem">
+    <input type="text" class="form-control" id="DefaultSide" aria-describedby="sideHelp" name="iItem">
+
+
     <div id="nameHelp" class="form-text">Enter the item you want to add.</div>
+    <div id="priceHelp" class="form-text">Enter the price.</div>
+    <div id="descHelp" class="form-text">Please provide a description.</div>
+    <div id="sideHelp" class="form-text">Enter the side it comes with.</div>
+
   </div>
   <button type="submit" class="btn btn-primary">Submit</button>
 </form>
