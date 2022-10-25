3cc08450ea2e15292fe598572ca5cc83af6cef11
Valerie Joplin <89474266+valeriejoplin@users.noreply.github.com>
2022-10-25 12:25:07 -0500

    Update food-add.php

--- food-add.php
+++ food-add.php
@@ -8,14 +8,14 @@ require_once("header.php");
     <input type="text" class="form-control" id="ItemName" aria-describedby="nameHelp" name="iItem">
     <div id="nameHelp" class="form-text">Enter the item you want to add.</div>
     <label for="Price" class="form-label">Price</label>
-    <input type="text" class="form-control" id="Price" aria-describedby="priceHelp" name="iItem"> 
+    <input type="text" class="form-control" id="Price" aria-describedby="priceHelp" name="iPrice"> 
     <div id="priceHelp" class="form-text">Enter the price.</div>
     <label for="Description" class="form-label">Description</label>
 
-    <input type="text" class="form-control" id="Description" aria-describedby="descHelp" name="iItem">
+    <input type="text" class="form-control" id="Description" aria-describedby="descHelp" name="iDesc">
     <div id="descHelp" class="form-text">Please provide a description.</div>
     <label for="DefaultSide" class="form-label">Side</label>
-    <input type="text" class="form-control" id="DefaultSide" aria-describedby="sideHelp" name="iItem">
+    <input type="text" class="form-control" id="DefaultSide" aria-describedby="sideHelp" name="iSide">
     <div id="sideHelp" class="form-text">Enter the side it comes with.</div>
 
   </div>
