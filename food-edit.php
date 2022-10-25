e1e152b99318f93c0b0b9588fa3a30096f08e4bc
Valerie Joplin <89474266+valeriejoplin@users.noreply.github.com>
2022-10-25 12:45:05 -0500

    Update food-edit.php

--- food-edit.php
+++ food-edit.php
@@ -16,9 +16,9 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "SELECT FoodID, ItemName from FoodMenu where FoodID=?";
+$sql = "SELECT FoodID, ItemName, Price, Description, DefaultSide from FoodMenu where FoodID=?";
 $stmt = $conn->prepare($sql);
-$stmt->bind_param("i", $_POST['iid']);
+$stmt->bind_param("i", $_POST['fid']);
 $stmt->execute();
 $result = $stmt->get_result();
 
@@ -28,11 +28,23 @@ if ($result->num_rows > 0) {
 ?>
 <form method="post" action="food-edit-save.php">
   <div class="mb-3">
-    <label for="ItemName" class="form-label">Name</label>
-    <input type="text" class="form-control" id="ItemName" aria-describedby="nameHelp" name="iITem" value="<?=$row['FoodItem']?>">
-    <div id="nameHelp" class="form-text">Enter the item name.</div>
+    <label for="ItemName" class="form-label">New Name</label>
+    <input type="text" class="form-control" id="ItemName" aria-describedby="nameHelp" name="iItem" value="<?=$row['ItemName']?>">
+    <div id="nameHelp" class="form-text">Enter the item's name</div>
+      <label for="FoodID" class="form-label">New Food ID</label>
+    <input type="text" class="form-control" id="FoodID" aria-describedby="nameHelp" name="iID" value="<?=$row['FoodID']?>">
+    <div id="nameHelp" class="form-text">Enter the Item's ID</div>
+      <label for="Price" class="form-label">Adjust Price</label>
+    <input type="text" class="form-control" id="Price" aria-describedby="nameHelp" name="iPrice" value="<?=$row['Price']?>">
+    <div id="nameHelp" class="form-text">Enter the Manager's name</div>
+         <label for="Description" class="form-label">Adjust Description</label>
+    <input type="text" class="form-control" id="Description" aria-describedby="nameHelp" name="iDesc" value="<?=$row['Description']?>">
+    <div id="nameHelp" class="form-text">Enter the description</div>
+         <label for="DefaultSide" class="form-label">Adjust Sides</label>
+    <input type="text" class="form-control" id="DefaultSide" aria-describedby="nameHelp" name="iSide" value="<?=$row['DefaultSide']?>">
+    <div id="nameHelp" class="form-text">Enter the side</div>
   </div>
-  <input type="hidden" name="iid" value="<?=$row['FoodID']?>">
+  <input type="hidden" name="fid" value="<?=$row['FoodID']?>">
   <button type="submit" class="btn btn-primary">Submit</button>
 </form>
 <?php
