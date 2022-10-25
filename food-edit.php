d52e939dc19e491f286113a34446e8e5c343f0de
Valerie Joplin <89474266+valeriejoplin@users.noreply.github.com>
2022-10-25 16:25:55 -0500

    Update food-edit.php

--- food-edit.php
+++ food-edit.php
@@ -16,7 +16,7 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "SELECT FoodID, ItemName, Price, Description, DefaultSide from FoodMenu where FoodID=?";
+$sql = "SELECT ItemName, Price, Description, DefaultSide from FoodMenu where FoodID=?";
 $stmt = $conn->prepare($sql);
 $stmt->bind_param("i", $_POST['fid']);
 $stmt->execute();
@@ -31,9 +31,6 @@ if ($result->num_rows > 0) {
     <label for="ItemName" class="form-label">New Name</label>
     <input type="text" class="form-control" id="ItemName" aria-describedby="nameHelp" name="iItem" value="<?=$row['ItemName']?>">
     <div id="nameHelp" class="form-text">Enter the item's name</div>
-      <label for="FoodID" class="form-label">New Food ID</label>
-    <input type="text" class="form-control" id="FoodID" aria-describedby="nameHelp" name="iID" value="<?=$row['FoodID']?>">
-    <div id="nameHelp" class="form-text">Enter the Item's ID</div>
       <label for="Price" class="form-label">Adjust Price</label>
     <input type="text" class="form-control" id="Price" aria-describedby="nameHelp" name="iPrice" value="<?=$row['Price']?>">
     <div id="nameHelp" class="form-text">Enter the Manager's name</div>
