dd612a2596eab38d989e521a0cb03afb3066acf4
Valerie Joplin <89474266+valeriejoplin@users.noreply.github.com>
2022-11-06 17:55:22 -0600

    Update employee-edit.php

--- employee-edit.php
+++ employee-edit.php
@@ -16,9 +16,9 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "SELECT * from Employees where EmployeeID=?";
+$sql = "SELECT FirstName, LastName, HireDate, Position from Employees where EmployeeID=?";
 $stmt = $conn->prepare($sql);
-$stmt->bind_param("i", $_POST['eID']);
+$stmt->bind_param("i", $_POST['eid']);
 $stmt->execute();
 $result = $stmt->get_result();
 
@@ -27,24 +27,21 @@ if ($result->num_rows > 0) {
   while($row = $result->fetch_assoc()) {
 ?>
 <form method="post" action="employee-edit-save.php">
- <div class="mb-3">
+  <div class="mb-3">
     <label for="FirstName" class="form-label">First Name</label>
-    <input type="text" class="form-control" id="FirstName" aria-describedby="nameHelp" name="eFirst">
-    <div id="nameHelp" class="form-text">Enter their first name.</div>
-    <label for="LastName" class="form-label">Last Name</label>
-    <input type="text" class="form-control" id="LastName" aria-describedby="nameHelp" name="eLast"> 
-    <div id="nameHelp" class="form-text">Enter their last name.</div>
-    <label for="HireDate" class="form-label">Hire Date</label>
-    <input type="text" class="form-control" id="HireDate" aria-describedby="nameHelp" name="eHireDate">
-    <div id="nameHelp" class="form-text">Enter their date of hire.</div>
-    <label for="Position" class="form-label">Position</label>
-    <input type="text" class="form-control" id="Position" aria-describedby="nameHelp" name="ePosition">
+    <input type="text" class="form-control" id="FirstName" aria-describedby="nameHelp" name="eFirst" value="<?=$row['FirstName']?>">
+    <div id="nameHelp" class="form-text">Enter their first name</div>
+      <label for="LastName" class="form-label">Last Name</label>
+    <input type="text" class="form-control" id="LastName" aria-describedby="nameHelp" name="eLast" value="<?=$row['LastName']?>">
+    <div id="nameHelp" class="form-text">Enter the Manager's name</div>
+         <label for="HireDate" class="form-label">Hire Date</label>
+    <input type="text" class="form-control" id="HireDate" aria-describedby="nameHelp" name="eHireDate" value="<?=$row['HireDate']?>">
+    <div id="nameHelp" class="form-text">Enter their date of hire</div>
+         <label for="Position" class="form-label">Position</label>
+    <input type="text" class="form-control" id="Position" aria-describedby="nameHelp" name="ePosition" value="<?=$row['Position']?>">
     <div id="nameHelp" class="form-text">Enter their job title(s).</div>
-    <label for="EmployeeID" class="form-label">ID</label>
-    <input type="text" class="form-control" id="EmployeeID" aria-describedby="nameHelp" name="eID"> 
-    <div id="nameHelp" class="form-text">Enter their ID number.</div>
   </div>
-  <input type="hidden" name="eID" value="<?=$row['EmployeeID']?>">
+  <input type="hidden" name="eid" value="<?=$row['EmployeeID']?>">
   <button type="submit" class="btn btn-primary">Submit</button>
 </form>
 <?php
